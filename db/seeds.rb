02e7cccff9dbe2c86a837205417f2bb632e21473
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-05-19 00:40:59 -0700

    seeding bd

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Topic.create(title: "Best Parks")
+user = User.create(:email => "someone@something.com", :password => "123455")
+topic = Topic.create(:title => "mytopic", :content "none")
+profile = Profile.create(:username => "pumpkin", :location=> "new jersey", :bio => "eh")
+
+Topic.create(user_id: user.id, topic_id: topic.id)
+Profile.create(:user_id: user.id)
